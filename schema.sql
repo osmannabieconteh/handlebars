f5abafc076338f3030e447b17bf7952e4a8a10cf
osman conteh <osmannabieconteh@gmail.com>
2019-11-06 21:32:11 -0500

    newchanges

---
+++ schema.sql
@@ -0,0 +1,15 @@
+
+DROP DATABASE IF EXISTS football_players_db;
+
+CREATE DATABASE football_players_db;
+
+USE football_players_db;
+
+-- 
+CREATE TABLE footballplayers (
+  id int NOT NULL AUTO_INCREMENT,
+  plan varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO football_players_db (plan) VALUES ('i want to ba a star.');
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8000;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Jjuniortoes.1",
+  database: "football_players_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
